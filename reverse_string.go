f4c93b7c5bec259f6d7e1acce7f2392cc6c469ce
stas-ik <stas-anikeyev@yandex.ru>
2022-05-30 22:24:31 +0300

    fix var naming

--- reverse_string.go
+++ reverse_string.go
@@ -1,7 +1,7 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	reverseStringRunes := []rune(string)
+	reverseStringRunes := []rune(input)
 	for i, j := 0, len(reverseStringRunes)-1; i < j; i, j = i+1, j-1 {
 		reverseStringRunes[i], reverseStringRunes[j] = reverseStringRunes[j], reverseStringRunes[i]
 	}
