b6ebdc5c22e8b2101d7e3c65a293e105df37d375
stas-ik <stas-anikeyev@yandex.ru>
2022-05-30 22:15:00 +0300

    string reverse ready

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,9 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
-	return output
+	reverseStringRunes := []rune(string)
+	for i, j := 0, len(reverseStringRunes)-1; i < j; i, j = i+1, j-1 {
+		reverseStringRunes[i], reverseStringRunes[j] = reverseStringRunes[j], reverseStringRunes[i]
+	}
+	return string(reverseStringRunes)
 }
